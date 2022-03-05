bd47d2d1f54dff106f00316deaf96439338ad36c
Ashishkatyal <40419086+AshishKatyal@users.noreply.github.com>
2022-03-05 16:01:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import nltk
 nltk.download('punkt')
 nltk.download('brown')
 
-
 import time
-import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
+
+image = Image.open('sentipy.jpg')
+
+st.image(image, use_column_width=True)
 
 
 # Fxn
@@ -20,25 +22,10 @@ def convert_to_df(sentiment):
 	sentiment_df = pd.DataFrame(sentiment_dict.items(),columns=['metric','value'])
 	return sentiment_df
 
-def analyze_token_sentiment(docx):
-	analyzer = SentimentIntensityAnalyzer()
-	pos_list = []
-	neg_list = []
-	neu_list = []
-	for i in docx.split():
-		res = analyzer.polarity_scores(i)['compound']
-		if res > 0.1:
-			pos_list.append(i)
-			pos_list.append(res)
-
-		elif res <= -0.1:
-			neg_list.append(i)
-			neg_list.append(res)
-		else:
-			neu_list.append(i)
-
-	result = {'positives':pos_list,'negatives':neg_list,'neutral':neu_list}
-	return result 
+def convert_to_df1(vs):
+	sentiment_dict1 = {'Positive':vs['pos']*100,'Negative':vs['neg']*100,'Neutral':vs['neu']*100}
+	sentiment_df1 = pd.DataFrame(sentiment_dict1.items(),columns=['metric','value'])
+	return sentiment_df1
 
 
 
@@ -51,34 +38,36 @@ def analyze_token_sentiment(docx):
 
 
 def main():
-	st.title("Pratikriya- A Sentiment Analysis NLP App")
-	st.subheader("Pratikriya- A Feedback Analysis Tool")
+	#st.title("Pratikriya- A Sentiment Analysis NLP App")
+	#st.subheader("Pratikriya- A Feedback Analysis Tool")
 
 	menu = ["Home","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
 
 	if choice == "Home":
 		st.subheader("Home")
+		#st.title("**Please Enter Your FeedBack**")
 		with st.form(key='nlpForm'):
-			raw_text = st.text_area("Enter Text Here")
+			raw_text = st.text_area("Please Enter Your Feedback")
 			submit_button = st.form_submit_button(label='Analyze')
-
+			
+			
 		# layout
 		col1,col2 = st.columns(2)
 		if submit_button:
 
 			with col1:
-				st.info("Results")
+				st.write("**Your Feedback has:**")
 				sentiment = TextBlob(raw_text).sentiment
-				st.write(sentiment)
+				#st.write(sentiment)
 
 				# Emoji
-				if sentiment.polarity > 0:
-					st.markdown("Sentiment:: Positive :smiley: ")
-				elif sentiment.polarity < 0:
-					st.markdown("Sentiment:: Negative :angry: ")
-				else:
-					st.markdown("Sentiment:: Neutral 😐 ")
+				#if sentiment.polarity > 0:
+					#st.markdown("Sentiment:: Positive :smiley: ")
+				#elif sentiment.polarity < 0:
+					#st.markdown("Sentiment:: Negative :angry: ")
+				#else:
+					#st.markdown("Sentiment:: Neutral 😐 ")
 
 				# Dataframe
 				result_df = convert_to_df(sentiment)
@@ -90,17 +79,61 @@ def main():
 					y='value',
 					color='metric')
 				st.altair_chart(c,use_container_width=True)
-
+				with st.expander("Polarity"):
+					 st.info("""
+					 The polarity score is a float within the range of -1.0 to 1.0. Where +1 means Positive Statement and -1 means Negative Statement""")
+				with st.expander("Subjectivity"):
+					 st.info("""
+					 The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective or refers to FACTUAL INFORMATION and 1.0 is very subjective. Subjective sentences generally refers to Personal Opinion, Emotions or Judgement""")
 
 
 			with col2:
-				st.info("Token Sentiment")
+				st.write("**Sentiment Analysis**")
+				analyzer = SentimentIntensityAnalyzer()
+				vs = analyzer.polarity_scores(raw_text)
+				
+				
 
-				token_sentiments = analyze_token_sentiment(raw_text)
-				st.write(token_sentiments)
+				
 
+				
+				#token_sentiments = analyze_token_sentiment(raw_text)
+				#st.write(token_sentiments)
+				# Dataframe
+				result_df1 = convert_to_df1(vs)
+				st.dataframe(result_df1)
 
+				# Visualization
+				c1 = alt.Chart(result_df1).mark_bar().encode(
+					x='metric',
+					y='value',
+					color='metric')
+				st.altair_chart(c1,use_container_width=True)
 
+				#st.write("Overall sentiment dictionary is : ", vs)
+				st.write("Feedback was rated as ", vs['neg']*100, "% Negative")
+				st.write("Feedback was rated as ", vs['neu']*100, "% Neutral")
+				st.write("Feedback was rated as ", vs['pos']*100, "% Positive")
+				st.write("**Overall Feedback Rated As**", end = " ")
+
+				# decide sentiment as positive, negative and neutral
+				if vs['compound'] >= 0.05 :
+					st.markdown("Positive :smiley: ")
+
+				elif vs['compound'] <= - 0.05 :
+					st.markdown("Negative :angry: ")
+
+				else :
+					st.markdown("Neutral 😐 ")
+
+				with st.expander("Hindi Translation"):
+					senti = TextBlob(raw_text)
+					st.text(senti.translate(to="hi"))
+					st.success('**Pratikriya** का उपयोग करने के लिए धन्यवाद')
+					#st.info("""
+					#The compound score is the sum of positive, negative & neutral scores which is then normalized between -1(most extreme negative) and +1 (most extreme positive). The more Compound score closer to +1, the higher the positivity of the text.""")
+
+			
 
 
 
